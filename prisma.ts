206acd1eb186091d32ef2eff383a55f4b2424933
seckin <contactseckin@gmail.com>
2025-07-19 04:47:19 +0300

    feat: neon adapter is implemented. Next steps ----> fix refresh token request in every login & return session

--- prisma.ts
+++ prisma.ts
@@ -1,8 +1,31 @@
-import { PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import { neonConfig, NeonConfig } from "@neondatabase/serverless";
 
-const globalPrisma = globalThis as unknown as { prisma: PrismaClient };
+import ws from "ws";
+neonConfig.webSocketConstructor = ws;
 
-export const prisma = globalPrisma.prisma || new PrismaClient();
+//? to work in edge environments (cloudflare workers, vercel, etc.) enable querying over fetch.
+// neonConfig.poolQueryViaFetch = true;
 
-//? In Prisma, Next.js or Serverless environment, always singleton is used.
-if (process.env.NODE_ENV !== "production") globalPrisma.prisma = prisma;
+//? Type definitions
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const adapter = new PrismaNeon({ connectionString });
+const prisma = global.prisma || new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
+
+export { prisma };
+//#region //? Old Prisma Client
+// const globalPrisma = globalThis as unknown as { prisma: PrismaClient };
+
+// export const prisma = globalPrisma.prisma || new PrismaClient();
+
+// //? In Prisma, Next.js or Serverless environment, always singleton is used.
+// if (process.env.NODE_ENV !== "production") globalPrisma.prisma = prisma;
+//#endregion
